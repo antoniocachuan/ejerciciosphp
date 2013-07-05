6631befeaf31a03b7a28a5a94e6cd5984462fe4f
Antonio Cachuan <alipazaga07@gmail.com>
2013-07-05 12:23:47 -0500

    agregado desde codeacademy

---
+++ ejerciciosresueltos.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+          // Here we define the function...
+          function helloWorld() {
+            echo "Hello world!";
+          }
+          
+          // ...and here we call it!
+          helloWorld();
+        ?>
+      </p>
+    </body>
+</html>
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        function greetings($name){
+            echo "Greetings, " . $name . "!";            
+            
+        }
+        
+        greetings ("antonio");
+        
+        ?>
+      </p>
+    </body>
+</html>
+
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        function aboutMe($name, $age){
+            echo "Hello! My name is $name, and I am $age years old, ";
+        }
+        aboutMe("Antonio", 15)
+        
+        ?>
+      </p>
+    </body>
+</html>
